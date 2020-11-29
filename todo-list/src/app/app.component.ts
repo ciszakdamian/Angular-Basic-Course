c96d3c67b6b690147b1d3f6eef51957d0603da37
Damian Ciszak <ciszakdamian@gmail.com>
2020-11-30 00:49:06 +0100

    interpolation

---
+++ todo-list/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-list';
+
+
+  get footer(): string {
+    return '2020 © Lista zadań,All rights reserved.';
+  }
+
+}
